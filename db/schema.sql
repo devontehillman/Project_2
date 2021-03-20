05100a51651768e0d3034fb63bd27c3e51c5f0c8
jaortisi6 <jaortisi@gmail.com>
2021-03-20 13:09:13 -0400

    updates to DB structure, authentication, and models

--- db/schema.sql
+++ db/schema.sql
@@ -1,40 +1,38 @@
-### Schema
-DROP DATABASE IF EXISTS thetechlife_db;
-CREATE DATABASE thetechlife_db;
+CREATE DATABASE IF NOT EXISTS thetechlife_db;
 USE thetechlife_db;
 
-CREATE TABLE users (
-    userfirstname VARCHAR(30) NOT NULL,
-    userlastname VARCHAR(30) NOT NULL,
-    userid INT auto_increment,
-    useremail VARCHAR(128) NOT NULL,
+CREATE TABLE user (
+    id INT auto_increment,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(128) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     admin Boolean,
     createdAt datetime,
     updatedAt datetime,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (userid)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE category (
-    categoryid int NOT NULL,
-    categoryname varchar(30) NOT NULL,
-    PRIMARY KEY (categoryid)
+    id int NOT NULL,
+    name varchar(30) NOT NULL,
+    PRIMARY KEY (id)
 
 );
 
 CREATE TABLE articles (
-    articleID int NOT NULL auto_increment,
-    articleTitle varchar(30) NOT NULL,
-    categoryID int NOT NULL,
-    articleEnglish text NOT NULL,
-    articleSpanish text, 
-    articleJapanese text,	
-    articleKorean text,
-    articleMandarin text,
-    articleImage blob,
-    authorID int,
-    PRIMARY KEY (articleID),
-    FOREIGN KEY (authorID) REFERENCES users(userid),
-    FOREIGN KEY (categoryID) REFERENCES category(CategoryID)
+    id int NOT NULL auto_increment,
+    title varchar(30) NOT NULL,
+    categoryId int NOT NULL,
+    english text NOT NULL,
+    spanishTrans text, 
+    japaneseTrans text,	
+    koreanTrans text,
+    mandarinTrans text,
+    articleImage VARCHAR(255),
+    authorId int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (authorId) REFERENCES user(id),
+    FOREIGN KEY (categoryId) REFERENCES category(id)
 
 );
