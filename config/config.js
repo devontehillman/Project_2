0829b06f9afdd78fd675e82fd6a887d019c2a042
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-03-22 16:33:00 -0400

    added dotenv package to make it easier for all devs to work on project

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: process.env.DB_PASS,
+    database: "thetechlife_db",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: process.env.DB_PASS,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    // eslint-disable-next-line camelcase
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
